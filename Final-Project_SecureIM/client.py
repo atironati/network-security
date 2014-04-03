86b84e3452d1fe7b90954121f33ed9b92ca51198
Alex Tironati <atironati@gmail.com>
2014-04-02 20:02:40 -0400

    Working dos cookie verification, setup for DH exchange

--- Final-Project_SecureIM/client.py
+++ Final-Project_SecureIM/client.py
@@ -12,6 +12,20 @@ if len(sys.argv) > 1:
 else:
     PORT = 9999
 
+g = 2
+p = """0xFFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
+         29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD
+         EF9519B3 CD3A431B 302B0A6D F25F1437 4FE1356D 6D51C245
+         E485B576 625E7EC6 F44C42E9 A637ED6B 0BFF5CB6 F406B7ED
+         EE386BFB 5A899FA5 AE9F2411 7C4B1FE6 49286651 ECE45B3D
+         C2007CB8 A163BF05 98DA4836 1C55D39A 69163FA8 FD24CF5F
+         83655D23 DCA3AD96 1C62F356 208552BB 9ED52907 7096966D
+         670C354E 4ABC9804 F1746C08 CA18217C 32905E46 2E36CE3B
+         E39E772C 180E8603 9B2783A2 EC07A28F B5C55DF0 6F4C52C9
+         DE2BCBF6 95581718 3995497C EA956AE5 15D22618 98FA0510
+         15728E5A 8AACAA68 FFFFFFFF FFFFFFFF"""
+p = int(p.replace(" ",""),0)
+
 class Client():
     # SOCK_DGRAM is the socket type to use for UDP sockets
     sock = None
@@ -23,10 +37,10 @@ class Client():
         # SOCK_DGRAM is the socket type to use for UDP sockets
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        self.greet_server()
+        self.login_to_server()
 
-    def greet_server(self):
-        self.send_helper("GREETING", "")
+    def login_to_server(self):
+        self.send_helper("LOGIN", "")
 
     def send_message(self, msg):
         self.send_helper("MESSAGE:", msg)
@@ -35,7 +49,18 @@ class Client():
     # checks for socket timeouts to detect inactive server
     def send_helper(self, prefix, msg):
         try:
+
             self.sock.sendto(prefix + msg, (HOST, PORT))
+            received = self.sock.recv(1024)
+            self.print_message("HEYHEY: " + received)
+            dos_cookie = received
+            self.print_message("dos_cookie: " + dos_cookie)
+
+            # compute diffie hellman value encrypted with password hash
+
+            encrypted_response_1 = "what"
+
+            self.sock.sendto("LOGIN," + dos_cookie + "," + encrypted_response_1, (HOST, PORT))
             received = self.sock.recv(1024)
             self.print_message(received)
 
@@ -45,6 +70,7 @@ class Client():
 
     # Print incoming messages to the terminal, has options for line breaks
     def print_message(self, msg, lb_before=False, lb_after=False):
+        print '\n' + msg
         if "INCOMING:" in msg:
             if lb_before:
                 print '\n' + msg[9:]
--- Final-Project_SecureIM/server.py
+++ Final-Project_SecureIM/server.py
@@ -15,6 +15,20 @@ from Crypto.Signature import PKCS1_v1_5
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto import Random # Much stronger than standard python random module
 
+g = 2
+p = """0xFFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
+         29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD
+         EF9519B3 CD3A431B 302B0A6D F25F1437 4FE1356D 6D51C245
+         E485B576 625E7EC6 F44C42E9 A637ED6B 0BFF5CB6 F406B7ED
+         EE386BFB 5A899FA5 AE9F2411 7C4B1FE6 49286651 ECE45B3D
+         C2007CB8 A163BF05 98DA4836 1C55D39A 69163FA8 FD24CF5F
+         83655D23 DCA3AD96 1C62F356 208552BB 9ED52907 7096966D
+         670C354E 4ABC9804 F1746C08 CA18217C 32905E46 2E36CE3B
+         E39E772C 180E8603 9B2783A2 EC07A28F B5C55DF0 6F4C52C9
+         DE2BCBF6 95581718 3995497C EA956AE5 15D22618 98FA0510
+         15728E5A 8AACAA68 FFFFFFFF FFFFFFFF"""
+p = int(p.replace(" ",""),0)
+
 BS    = 16
 pad   = lambda s : s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
 unpad = lambda s : s[0:-ord(s[-1])]
@@ -26,12 +40,13 @@ server_pub_key  = RSA.importKey(open('server_pub_key.txt', 'r').read().rstrip('\
 server_priv_key = RSA.importKey(open('server_priv_key.txt', 'r').read().rstrip('\n'))
 
 server_secret = Random.new().read( 32 )
+iv            = Random.new().read( 16 )
 
 conn = sqlite3.connect('server_db.db')
 c = conn.cursor()
 
-pub_key   = RSA.importKey(open('cryptography/alice_pub.txt', 'r').read().rstrip('\n'))
-priv_key  = RSA.importKey(open('cryptography/alice_priv.txt', 'r').read().rstrip('\n'))
+#pub_key   = RSA.importKey(open('cryptography/alice_pub.txt', 'r').read().rstrip('\n'))
+#priv_key  = RSA.importKey(open('cryptography/alice_priv.txt', 'r').read().rstrip('\n'))
 
 # c.execute("INSERT INTO users (ip, password_hash, pub_key, priv_key,name) VALUES ('129.10.9.112','alex','" + pub_key.exportKey() + "','" + priv_key.exportKey() + "','alex')")
 conn.commit()
@@ -60,31 +75,48 @@ class UDPHandler(SocketServer.BaseRequestHandler):
 
         if data_len > 0 and data[0] == "LOGIN":
             #self.register_client(self.client_address)
-            #print "Client registered: " + str(self.client_address)
+            print "Client registered: " + str(self.client_address)
             if data_len == 1:
-                send_dos_cookie()
+                self.send_dos_cookie()
             elif data_len == 3:
-                if verify_dos_cookie(data[1]):
+                if self.verify_dos_cookie(data[1]):
                     diff_hell_val = RSA_decrypt_login(data[2])
-                    if diff_hell_val is not None:
-                        authenticating_clients[str(self.client_address)]['user_diff_hell_val'] = diff_hell_val
-                        #send client our diff hell val encoded with the password and whatever
+                    print diff_hell_val
+                    #if diff_hell_val is not None:
+                        #authenticating_clients[str(self.client_address)]['user_diff_hell_val'] = diff_hell_val
+                        ##send client our diff hell val encoded with the password and whatever
 
-            self.socket.sendto("INCOMING:You are connected to the chat server!",
-                               self.client_address)
+            #self.socket.sendto("INCOMING:You are connected to the chat server!",
+            #                   self.client_address)
 
         elif data.startswith("MESSAGE:"):
             self.broadcast_message(data[8:])
 
     def send_dos_cookie(self):
-        client_ip = str(self.client_address)
-        iv        = Random.new().read( 16 )
+        client_ip = str(self.client_address[0])
 
-        plaintext  = pad(client_ip)
+        plaintext  = pad(client_ip + "," + str(server_secret))
         cipher     = AES.new(server_secret, AES.MODE_CBC, iv)
         ciphertext = base64.b64encode(cipher.encrypt(plaintext))
 
-        self.socket.sendto(ciphertext)
+        self.socket.sendto(ciphertext, self.client_address)
+
+    def verify_dos_cookie(self, cookie):
+        try:
+            cipher    = AES.new(server_secret, AES.MODE_CBC, iv)
+            plaintext = unpad(cipher.decrypt(base64.b64decode( cookie )))
+            plaintext = plaintext.split(',')
+            if len(plaintext) == 2:
+                client_ip  = plaintext[0]
+                our_secret = plaintext[1]
+                if client_ip == self.client_address[0] and server_secret == our_secret:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        except TypeError:
+            return False
 
     def RSA_decrypt_login(ciphertext):
         encrypted_msg_key = base64.b64decode( line1 )
